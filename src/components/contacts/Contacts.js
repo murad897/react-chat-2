918560a5a1ac71f6295931182b03d78f124a44fa
user <m.jamburia.websmart@gmail.comgit config --global user.email m.jamburia.websmart@gmail.com>
2022-06-28 18:09:57 +0400

    add popover edit user

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Popover from "@mui/material/Popover";
 
 const Contacts = ({ contacts, personName, changeChat, currentUser }) => {
   const [token, setToken] = useState("");
   const [currentSelected, setCurrentSelected] = useState(undefined);
   const [currrentuserName, setCurrrentUserName] = useState(undefined);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
 
   const changeCurrrentChat = (index, contact) => {
     setCurrentSelected(index);
@@ -32,6 +45,25 @@ const Contacts = ({ contacts, personName, changeChat, currentUser }) => {
       </div>
       <div className="current-user">
         <p>{personName}</p>
+        <button className="edit-button" onClick={handleClick}>
+          edit
+        </button>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+        >
+          <form>
+            edit name : <input type="text" />
+            edit pic: <input type="file" id="img" name="img" accept="image/*" />
+            <input type="submit" />
+          </form>
+        </Popover>
       </div>
     </div>
   );
